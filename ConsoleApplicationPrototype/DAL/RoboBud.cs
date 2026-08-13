04f49e3d252f9994234aec50c027c31394a426f5
Alison Emilien <aemil072@uottawa.ca>
2026-08-13 13:16:27 +0400

    Replace repository with local project

---
+++ ConsoleApplicationPrototype/DAL/RoboBud.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL;
+
+public class RoboBud
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double HappinessLevel { get; set; }
+
+    public RoboBud(string name, int age, double happinessLevel)
+    {
+        Name = name;
+        Age = age;
+        HappinessLevel = happinessLevel;
+    }
+}
